4f15204c80b82d80e2ba08915ab72db838bfd171
Christian Monrabal <christianmonrabaldonis@gmail.com>
2025-09-26 21:49:39 +0200

    new route index

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>FlatMate</title>
+</head>
+<body>
+    <h1>Welcome to FlatMate</h1>
+    <p>Your go-to platform for finding the perfect flatmate!</p>
+</body>
+</html>
